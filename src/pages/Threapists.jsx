84219bd02267c2280731a70f1ae136b452fc2627
Nishant Rana <126577697+NishantRana07@users.noreply.github.com>
2025-09-30 10:54:51 +0530

    Update Threapists.jsx

--- src/pages/Threapists.jsx
+++ src/pages/Threapists.jsx
@@ -97,75 +97,7 @@ const Threapists = () => {
 
         </div>
 
-        {/* Therapist Card */}
-        <div className="bg-red-200 relative h-68 w-58 flex-none rounded-sm overflow-hidden hover:border-red-400 border-2 hover:-translate-y-2 transition  duration-500 ease-in-out"> 
-            <img src={IMG} className="h-full w-full object-cover" alt="therapist image" />
-            <div className="absolute inset-0 bg-black/20 backdrop-blur-sm hover:backdrop-blur-none transition duration-500 ease-in-out"></div>
-            {/* Details & Button Section */}
-            <div className="absolute bottom-0 left-0 w-full bg-red-200 bg-opacity-80 flex justify-between items-center gap-2 p-2">
-                {/* Details */}
-                <div className="text-black text-[10px] p-1 text-start font-semibold">
-                <p>Name: Nishant</p>
-                <p>Description: Nishant is not good at talking to ladiz</p>
-                </div>
-
-                {/* Button */}
-                <button className="text-[10px] bg-blue-600  w-36 text-white rounded flex items-center gap-1 px-2 py-1">
-                <img src={icon} className="h-4 w-4" alt="icon" />
-                <p>Connect</p>
-                </button>
-
-            </div>
-            <img src={IMG} className="h-full w-full object-cover" alt="therapist image z-10" />
-
-        </div>
-
-
-        {/* Therapist Card */}
-        <div className="bg-red-200 relative h-68 w-58 flex-none rounded-sm overflow-hidden hover:border-red-400 border-2 hover:-translate-y-2 transition  duration-500 ease-in-out"> 
-            <img src={IMG} className="h-full w-full object-cover" alt="therapist image" />
-            <div className="absolute inset-0 bg-black/20 backdrop-blur-sm hover:backdrop-blur-none transition duration-500 ease-in-out"></div>
-            {/* Details & Button Section */}
-            <div className="absolute bottom-0 left-0 w-full bg-red-200 bg-opacity-80 flex justify-between items-center gap-2 p-2">
-                {/* Details */}
-                <div className="text-black text-[10px] p-1 text-start font-semibold">
-                <p>Name: Nishant</p>
-                <p>Description: Nishant is not good at talking to ladiz</p>
-                </div>
-
-                {/* Button */}
-                <button className="text-[10px] bg-blue-600  w-36 text-white rounded flex items-center gap-1 px-2 py-1">
-                <img src={icon} className="h-4 w-4" alt="icon" />
-                <p>Connect</p>
-                </button>
-
-            </div>
-            <img src={IMG} className="h-full w-full object-cover" alt="therapist image z-10" />
-
-        </div>
-
-        {/* Therapist Card */}
-        <div className="bg-red-200 relative h-68 w-58 flex-none rounded-sm overflow-hidden hover:border-red-400 border-2 hover:-translate-y-2 transition  duration-500 ease-in-out"> 
-            <img src={IMG} className="h-full w-full object-cover" alt="therapist image" />
-            <div className="absolute inset-0 bg-black/20 backdrop-blur-sm hover:backdrop-blur-none transition duration-500 ease-in-out"></div>
-            {/* Details & Button Section */}
-            <div className="absolute bottom-0 left-0 w-full bg-red-200 bg-opacity-80 flex justify-between items-center gap-2 p-2">
-                {/* Details */}
-                <div className="text-black text-[10px] p-1 text-start font-semibold">
-                <p>Name: Nishant</p>
-                <p>Description: Nishant is not good at talking to ladiz</p>
-                </div>
-
-                {/* Button */}
-                <button className="text-[10px] bg-blue-600  w-36 text-white rounded flex items-center gap-1 px-2 py-1">
-                <img src={icon} className="h-4 w-4" alt="icon" />
-                <p>Connect</p>
-                </button>
-
-            </div>
-            <img src={IMG} className="h-full w-full object-cover" alt="therapist image z-10" />
-
-        </div>
+        
         </div>
         <button onClick={scrollRight} className=" mt-auto mb-auto text-white bg-transparent bg-blur absolute z-10 top-1/2 right-5">
 
@@ -181,3 +113,4 @@ const Threapists = () => {
 
 
 export default Threapists
+
